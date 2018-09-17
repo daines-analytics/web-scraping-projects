bf3d48acda54e8edbfa0e171a58a0e4e7b6a9c10
David Lowe <david.lowe@dainesanalytics.com>
2018-09-17 10:26:11 -0700

    Initial upload of the Famous People Quotes web scraping Python script Take 3

---
+++ web-scraping-famous-quotes-take3/scroll_spider.py
@@ -0,0 +1,22 @@
+import json
+import scrapy
+
+class ScrollSpider(scrapy.Spider):
+    name = "scroll"
+    api_url = 'http://quotes.toscrape.com/api/quotes?page={}'
+    start_urls = [api_url.format(1)]
+
+    def parse(self, response):
+        data = json.loads(response.text)
+        for quote in data['quotes']:
+            yield {
+                'author_name': quote['author']['name'],
+                'text': quote['text'],
+                'tags': quote['tags'],
+                'author_url': quote['author']['goodreads_link'],
+            }
+
+        # follow pagination link
+        if data['has_next']:
+            next_page = data['page'] + 1
+            yield scrapy.Request(url=self.api_url.format(next_page), callback=self.parse)
